8af33ed7eaf7b590efced1a3e4f1ae232473b26c
Gal Akerman <akermang@gmail.com>
2019-04-17 21:41:20 +0300

    Updates

---
+++ precache-manifest.2977b356311fabefa58c2c5efe0df4e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "124f53d2f4f46934a59a2705f94046c2",
+    "url": "/static/media/logo.124f53d2.svg"
+  },
+  {
+    "revision": "284d1e59d89ffa0d2475e3bd746d9a6b",
+    "url": "/static/media/back.284d1e59.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a133d2974367749cb69",
+    "url": "/static/js/main.4317cd72.chunk.js"
+  },
+  {
+    "revision": "8e23dd9474b7fcea16a3",
+    "url": "/static/js/2.bca43d97.chunk.js"
+  },
+  {
+    "revision": "6a133d2974367749cb69",
+    "url": "/static/css/main.472f6ccb.chunk.css"
+  },
+  {
+    "revision": "13beecec6b99394969eae554cfa96a2d",
+    "url": "/index.html"
+  }
+];
